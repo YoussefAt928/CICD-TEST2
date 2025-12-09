55b13bcfb239322cc9d399fbec36c9e1f9633f28
YoussefAt928 <youssef@reyadabusiness.com>
2025-12-09 14:28:13 +0200

    Test CI/CD deployment to test env

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -116,7 +116,7 @@
 </head>
 
 <body>
-    <h1>رفع ملف CSV4</h1>
+    <h1>رفع ملف CSV5</h1>
 
     @if (session('success'))
         <p style="color: #34c759; font-weight: bold;">{{ session('success') }}</p>
